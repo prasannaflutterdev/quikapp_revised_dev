ec9120bea315b25ebab4ba8fb5720931a3f26ee6
QuikAppTech <prasannasrie@gmail.com>
2025-05-17 12:45:31 +0530

    Android Release 39

--- android/app/src/main/kotlin/com/example/app/Application.kt
+++ android/app/src/main/kotlin/com/example/app/Application.kt
@@ -1,10 +1,19 @@
 package com.example.app
 
-import io.flutter.app.FlutterApplication
 import androidx.multidex.MultiDexApplication
+import io.flutter.app.FlutterApplication
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class Application : MultiDexApplication() {
+class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        if (registry != null) {
+            GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+        }
     }
 } 
